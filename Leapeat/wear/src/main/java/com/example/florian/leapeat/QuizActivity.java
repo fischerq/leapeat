fb7520b0591c6e2c15fa62d448ff60aeb95ca5a6
Tecqu <takutecq@gmail.com>
2014-11-08 14:22:57 +0100

    quiz added

--- Leapeat/wear/src/main/java/com/example/florian/leapeat/QuizActivity.java
+++ Leapeat/wear/src/main/java/com/example/florian/leapeat/QuizActivity.java
@@ -1,20 +1,18 @@
 package com.example.florian.leapeat;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
-import android.view.View;
 import android.widget.TextView;
 
-public class LearnActivity extends Activity {
+public class QuizActivity extends Activity {
 
     private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_learn);
+        setContentView(R.layout.activity_quiz);
         final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
             @Override
@@ -23,14 +21,4 @@ public class LearnActivity extends Activity {
             }
         });
     }
-
-    public void startQuiz(View view)
-    {
-        Intent intent = new Intent(this, QuizActivity.class);
-        //EditText editText = (EditText) findViewById(R.id.edit_message);
-        //String message = editText.getText().toString();
-        //intent.putExtra("MESSAGE", message);
-        startActivity(intent);
-    }
-
 }
